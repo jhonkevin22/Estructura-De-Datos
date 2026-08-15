a939a98b79a8b85aa58bca1e647a8576f2e529cc
jhonkevin22 <jhonpupi22@gmail.com>
2026-08-14 19:57:17 -0500

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,25 @@
 
+def sumar(num1, num2):
+    return num1 + num2
+
+def restar(num1, num2):
+    return num1 - num2
+
+def multiplicar(num1, num2):
+    return num1 * num2
+
+def dividir(num1, num2):
+    if num2 == 0:
+        return "no se puede dividir por 0"
+    return num1 / num2
+def main():
+    num1 = 50
+    num2 = 0
+
+    print("sumar:", sumar(num1, num2))
+    print("restar:", restar(num1, num2))
+    print("multiplicar:", multiplicar(num1, num2))
+    print("dividir:", dividir(num1, num2))
+
+if __name__== "__main__":
+    main()
