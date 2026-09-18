8022a4383c02315fbf28a20710234f39b748fc8e
jhonkevin22 <jhonpupi22@gmail.com>
2026-09-18 13:06:06 -0500

    Create vectores.cs

---
+++ vectores.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[5]
+                {11, 23, 223, 43, 13};
+             Console.WriteLine ($"el elemento en [0]: {numeros[4]}");
+        }
+    }
+}
